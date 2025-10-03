ffc5209756c807c21cb2f7467c7d6d54cd0de35f
Lê Hồ Chí Bảo <baoproa6@gmail.com>
2025-10-03 08:44:00 +0700

    Delete SP_validator_Interceptor- directory

--- SP_validator_Interceptor-/src/main/java/LTW_GraphQL/Model/CategoryInput.java
+++
@@ -1,9 +0,0 @@
-package LTW_GraphQL.Model;
-
-import lombok.Data;
-
-@Data
-public class CategoryInput {
-    private String name;
-    private String images;
-}
--- SP_validator_Interceptor-/src/main/java/LTW_GraphQL/Model/Response.java
+++
@@ -1,7 +0,0 @@
-package LTW_GraphQL.Model;
-
-public class Response {
-	private Boolean status; 
-	private String message; 
-	private Object body; 
-}
